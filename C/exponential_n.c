79cfc275afeaabffd3c2d499fdb80ba296ce2a9c
bornabhowmik <bornabhowmik4@gmail.com>
2024-01-08 00:02:54 +0600

    exponential_n.c code added

---
+++ C/exponential_n.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+
+    double number=21.3e9;
+
+    printf("%lf", number);
+
+    return 0;
+}
--- C/float2.c
+++ C/float2.c
@@ -4,6 +4,6 @@ int main(){
     float number=3451.6547;
 
     printf("%.1f", number);
-    
+
     return 0;
 }
