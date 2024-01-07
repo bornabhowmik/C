a2b196aafb6ef1d125fcaf1c52cb9461a175e75b
bornabhowmik <bornabhowmik4@gmail.com>
2024-01-07 21:20:50 +0600

    add_v2.c code added

---
+++ C/add_v2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int firstNumber=21;
+
+    printf("FirstNumber: %d", firstNumber);
+
+    int secondNumber=31;
+
+    printf("\nSecondNumber: %d", secondNumber);
+
+    return 0;
+}
