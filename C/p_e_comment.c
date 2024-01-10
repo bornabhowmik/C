8b6a69920f8131eada49f2e89e17bad489a3cd9d
bornabhowmik <bornabhowmik4@gmail.com>
2024-01-10 10:36:01 +0600

    p_e_comment.c code added

---
+++ C/p_e_comment.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    int age;
+    double hight;
+
+    printf("Enter the value of age: ");
+    scanf("%d", &age);
+
+    // printf("Enter the value of hight: ");
+    // scanf("%lf", &hight);
+
+    printf("Age: %d", age);
+    // printf("\nHight: %lf", hight);
+
+    return 0;
+}
