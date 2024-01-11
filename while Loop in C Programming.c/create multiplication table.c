e810325a2b9399ea7bd7aa866d84cb0dd41a1933
bornabhowmik <bornabhowmik4@gmail.com>
2024-01-11 14:01:54 +0600

    update code added

--- while Loop in C Programming.c/create multiplication table.c
+++ while Loop in C Programming.c/create multiplication table.c
@@ -12,5 +12,6 @@ int main() {
         printf("%d * %d = %d\n", number,count,answer);
         count = count + 1;
     }
+    
     return 0;
 }
--- while Loop in C Programming.c/infinite while loop.c
+++ while Loop in C Programming.c/infinite while loop.c
@@ -4,5 +4,6 @@ int main() {
     while(1<5){
         printf("while loop in c\n");
     }
+    
     return 0;
 }
